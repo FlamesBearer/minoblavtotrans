7d0cc4ea7e00435f4f8d10ec8fea51e950ebc2b3
FlamesBearer <61793797+FlamesBearer@users.noreply.github.com>
2020-06-15 20:05:02 +0300

    Add files via upload
    v1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.main-slider_body').slick({
+		arrows: false,
+		dots: true,
+		speed: 1000,
+		easing:'ease',
+		autoplay: true,
+		autoplaySpeed: 3500
+	});
+});
